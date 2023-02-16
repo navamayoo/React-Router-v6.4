0048445b72fac148e07c798a666fac9277968d56
Mayoorann <navamayoo@gmail.com>
2023-02-16 16:47:01 +0530

    add Breadcrumb

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,12 +1,17 @@
 import { Container } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import AppBreadcrumbs from "../components/AppBreadcrumbs";
 import Navbar from "../components/Navbar";
 
 export default function RootLayout() {
   return (
     <>
       <Navbar />
+      <Container sx={{ p: 3 }}>
+        <AppBreadcrumbs />
+      </Container>
+
       <Container sx={{ p: 10 }}>
         <Outlet />
       </Container>
