37b5fa92c9ed48553bfd5c9cd9e6dcc65eb6b264
Mayoorann <navamayoo@gmail.com>
2023-02-16 13:09:51 +0530

    create Browser Router

--- react-router-v6/src/App.js
+++ react-router-v6/src/App.js
@@ -1,10 +1,37 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Router,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
-import Navbar from "./layouts/Navbar";
 
+import RootLayout from "./layouts/RootLayout";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+import ContactLayout from "./layouts/ContactLayout";
+import Mail from "./pages/Mail";
+import Phone from "./pages/Phone";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<ContactLayout />}>
+          <Route path="mail" element={<Mail />} />
+          <Route path="phone" element={<Phone />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    )
+  );
   return (
     <>
-      <Navbar />
+      <RouterProvider router={router} />
     </>
   );
 }
--- react-router-v6/src/pages/About.jsx
+++ react-router-v6/src/pages/About.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import { Typography } from "@mui/material";
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <>
+      <Typography variant="h4">About</Typography>
+      <Typography variant="h6"> What is Lorem Ipsum? </Typography>
+      <Typography variant="body">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.{" "}
+        <Typography variant="h6"> Why do we use it? </Typography> It is a long
+        established fact that a reader will be distracted by the readable
+        content of a page when looking at its layout. The point of using Lorem
+        Ipsum is that it has a more-or-less normal distribution of letters, as
+        opposed to using 'Content here, content here', making it look like
+        readable English. Many desktop publishing packages and web page editors
+        now use Lorem Ipsum as their default model text, and a search for 'lorem
+        ipsum' will uncover many web sites still in their infancy. Various
+        versions have evolved over the years, sometimes by accident, sometimes
+        on purpose (injected humour and the like). Where does it come from?
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC, making it
+        over 2000 years old.
+      </Typography>
+    </>
+  );
 }
