31a2f89b7b4efc2217957418dd4c3e6bcd251e23
Mayoorann <navamayoo@gmail.com>
2023-05-31 15:08:36 +0530

    sg

--- src/components/AppBreadcrumbs.jsx
+++ src/components/AppBreadcrumbs.jsx
@@ -4,19 +4,16 @@ import { useLocation, Link } from "react-router-dom";
 
 const AppBreadcrumbs = () => {
   const location = useLocation();
-  ///contact/mail
+
   let crumbLink = "";
-  const crumbPath = location.pathname
-    .split("/")
-    .filter((path) => path !== "")
-    .map((crumb) => {
-      crumbLink += `/${crumb}`;
+  const crumbPath = location.pathname.split("/").filter((path) => path !== "").map((crumb) => {crumbLink += `/${crumb}`;
       return (
         <Link to={crumbLink} key={crumb}>
           {crumb}
         </Link>
       );
     });
+
   console.log(crumbPath);
   return <Breadcrumbs aria-label="breadcrumb">{crumbPath}</Breadcrumbs>;
 };
